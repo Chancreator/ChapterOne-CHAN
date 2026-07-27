6aa1406609e4877d5ac01a397cfd6cd3779c43b3
Chancreator <ps.christianlloyd@gmail.com>
2026-07-27 09:36:10 +0800

    Add basic widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:chapterone/main.dart';
+
+void main() {
+  testWidgets('ChapterOne app builds without crashing', (WidgetTester tester) async {
+    await tester.pumpWidget(const ChapterOneApp());
+    // The bookshelf home screen should show the app title in the AppBar.
+    expect(find.text('ChapterOne'), findsOneWidget);
+  });
+}
